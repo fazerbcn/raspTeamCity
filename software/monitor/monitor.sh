fc951582c5826995167e8c19b17120ec4990cf09
Pau Ruiz <pau@iosnow.net>
2015-01-22 12:24:02 +0000

    Created shell script to run php monitor

--- software/monitor/monitor.sh
+++ software/monitor/monitor.sh
@@ -1,22 +1,4 @@
-#!/bin/sh
-# We will parse all variables of the config file
-shopt -s extglob
-configfile="raspTeamCity.conf" # set the actual path name of your (DOS or Unix) config file
-tr -d '\r' < $configfile > $configfile.unix
-while IFS='= ' read lhs rhs
-do
-    if [[ ! $lhs =~ ^\ *# && -n $lhs ]]; then
-        rhs="${rhs%%\#*}"    # Del in line right comments
-        rhs="${rhs%%*( )}"   # Del trailing spaces
-        rhs="${rhs%\"*}"     # Del opening string quotes 
-        rhs="${rhs#\"*}"     # Del closing string quotes 
-        declare $lhs="$rhs"
-    fi
-done < $configfile.unix
-
-# We will download all projects information from TeamCity
-echo "HOLA$teamcityUrl"
-#wget $teamcityUrl
-# We will convert the XML file to JSON
-# We will get the state of the project to monitor
-# We will control the gpio according to the state of the project we are monitoring
+!/bin/sh
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+echo $DIR
+php $DIR/monitor.php
